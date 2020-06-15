5d23219f2c17f171c98a85ef8056dc2eac34ffef
JDzhambazov <jamby@mail.bg>
2020-06-15 12:58:31 +0300

    Connect to Momgo Atlas

--- Cubicle - MongoDB/config/config.js
+++ Cubicle - MongoDB/config/config.js
@@ -1,6 +1,9 @@
+const DB_PASSWORD=require('../dbPass')
+
 module.exports = {
     development: {
-        port: process.env.PORT || 3000
+        port: process.env.PORT || 3000 ,
+        databaseUrl:`mongodb+srv://JUser:${DB_PASSWORD.DB_PASSWORD}@jtests-1kakw.mongodb.net/cubicle?retryWrites=true&w=majority`
     },
     production: {}
 };
--- Cubicle - MongoDB/index.js
+++ Cubicle - MongoDB/index.js
@@ -1,10 +1,24 @@
 const env = process.env.NODE_ENV || 'development';
 global.__appPath = __dirname;
+const mongoose = require('mongoose')
 
 const config = require('./config/config')[env];
 const app = require('express')();
 const cubeModel = require('./models/cubemodel')
 
+mongoose.connect(config.databaseUrl ,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err)=>{
+    if(err){
+        console.error(err);
+        throw err;
+    }
+
+    console.log('Database is runing')
+})
 
 require('./config/express')(app);
 require('./config/routes')(app);
