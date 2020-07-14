0ea01ffe5439cfda7291f09e4396aadbced42a17
JDzhambazov <jamby@mail.bg>
2020-07-14 17:54:15 +0300

    opit

--- Cubicle - MongoDB/controllers/accessory.js
+++ Cubicle - MongoDB/controllers/accessory.js
@@ -2,7 +2,6 @@ const Accessory = require('../models/accessories');
 
 const getAllAccessory = async () =>{
     const accessories = await Accessory.find().lean();
-    console.log(accessories)
     return accessories
 };
 
